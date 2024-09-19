96898c059e8c38af115fce72d266e056f4648161
R4k4210 <elchopi26@gmail.com>
2024-09-19 00:02:04 -0300

    feat: added copilot removed codeium

---
+++ lua/r4k4210/plugins/copilot.lua
@@ -0,0 +1,7 @@
+return {
+  "github/copilot.vim",
+  config = function()
+    -- vim.g.copilot_assume_mapped = true
+    vim.api.nvim_set_keymap("i", "<C-s>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+  end,
+}
